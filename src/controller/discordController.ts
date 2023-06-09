ba6ef72fbbfb6fa2f0e397c12fd03de954374524
umarsabra <omaarsaabra@outlook.com>
2023-06-09 06:52:51 +0300

    created route

--- src/controller/discordController.ts
+++ src/controller/discordController.ts
@@ -39,14 +39,14 @@ router.post("/reminder", (req, res) => {
   const webhookUrl = process.env.DISCORD_REMINDER_WEBHOOK_URL;
 
   console.log(req.body);
-  // const data: ApptReminderMessageData = {
-  //   firstName: req.body.fist_name as unknown as string,
-  //   clientName: req.body.client_name as unknown as string,
-  //   apptDate: req.body.appt_date as unknown as string,
-  // };
+  const data: ApptReminderMessageData = {
+    firstName: req.body.fist_name as unknown as string,
+    clientName: req.body.client_name as unknown as string,
+    apptDate: req.body.appt_date as unknown as string,
+  };
 
-  // const content = createApptCloserReminder(data);
-  // sendMessage(content, webhookUrl);
+  const content = createApptCloserReminder(data);
+  sendMessage(content, webhookUrl);
   res.send("message sent");
 });
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import discordController from "./controller/discordController";
+import bodyParser from "body-parser";
 
 dotenv.config();
 const PORT = process.env.PORT as unknown as number;
 const IP = process.env.IP;
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use("/discord", discordController);
 
 app.get("/", (req, res) => {
